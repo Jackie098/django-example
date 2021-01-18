8a3f485c70f1b86a0b967efcef1b430eb06a864f
Carlos A <carlosmiranda19122@gmail.com>
2021-01-18 17:13:47 -0300

    learning to use 'admin' and integration with database

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 
-class Student:
-  def __init__(self, registration=None, name=None, scholarship=False): #If it has no value, NONE is the default
-    self.registration = registration
-    self.name = name
-    self.scholarship = scholarship
+class Student(models.Model):
+  # def __init__(self, registration=None, name=None, scholarship=False): #If it has no value, NONE is the default
+    registration = models.IntegerField(unique=True)
+    name = models.CharField(max_length=50)
+    scholarship = models.BooleanField(default=False)
 
     def __str__(self):
       return 'registration={}, name={}, scholarship={}'.format(self.registration, self.name, self.scholarship)
